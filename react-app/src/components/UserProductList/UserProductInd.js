490d5e1060ddb7acbe28ac9ef21f1915c331f0ef
fangruz114 <101694480+fangruz114@users.noreply.github.com>
2022-09-07 10:47:04 -0700

    fix modal background due to multiple modals open

---
+++ react-app/src/components/UserProductList/UserProductInd.js
@@ -0,0 +1,38 @@
+import { Modal } from '../../context/Modal';
+import { Link } from 'react-router-dom';
+import DeleteProductConf from './DeleteProductConf';
+import { useState } from 'react';
+
+function UserProductInd({ product }) {
+    const [showConfirmationModel, setShowConfirmationModel] = useState(false);
+
+    return (
+        <div className='user-products-ind' key={product.id}>
+            <div className='user-product-img'>
+                <Link to={`/products/${product.id}`}>
+                    <img src={product.images[0].url} alt='user-product-display' />
+                </Link>
+            </div>
+            <div className='user-product-info'>
+                <Link to={`/products/${product.id}`}>
+                    <p className='user-product-name'>{product.name}</p>
+                </Link>
+                <p className='user-product-number'>Item #: {product.id}</p>
+                <p>${product.price}</p>
+            </div>
+            <div className='user-product-edit-delete-btn'>
+                <Link to={`/products/${product.id}/edit`}>EDIT</Link>
+                <button onClick={() => setShowConfirmationModel(true)}>DELETE</button>
+                {showConfirmationModel && (
+                    <div className='delete-product-confirm-modal'>
+                        <Modal onClose={() => setShowConfirmationModel(false)} id={product.id}>
+                            <DeleteProductConf onClose={() => setShowConfirmationModel(false)} id={product.id} />
+                        </Modal>
+                    </div>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default UserProductInd;
--- react-app/src/components/UserProductList/index.js
+++ react-app/src/components/UserProductList/index.js
@@ -1,15 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getUserProducts } from '../../store/product';
-import { Modal } from '../../context/Modal';
-import DeleteProductConf from './DeleteProductConf';
+import UserProductInd from './UserProductInd';
 import './UserProductList.css';
 
 function UserProductList() {
     const dispatch = useDispatch();
     const userProducts = useSelector(state => Object.values(state.products));
-    const [showModal, setShowModal] = useState(false);
 
     useEffect(() => {
         dispatch(getUserProducts())
@@ -27,31 +25,7 @@ function UserProductList() {
                 <div className='user-product-list'>
                     {userProducts.length > 0 && (
                         userProducts.map(product => (
-                            <div className='user-products-ind' key={product.id}>
-                                <div className='user-product-img'>
-                                    <Link to={`/products/${product.id}`}>
-                                        <img src={product.images[0].url} alt='user-product-display' />
-                                    </Link>
-                                </div>
-                                <div className='user-product-info'>
-                                    <Link to={`/products/${product.id}`}>
-                                        <p className='user-product-name'>{product.name}</p>
-                                    </Link>
-                                    <p className='user-product-number'>Item #: {product.id}</p>
-                                    <p>${product.price}</p>
-                                </div>
-                                <div className='user-product-edit-delete-btn'>
-                                    <Link to={`/products/${product.id}/edit`}>EDIT</Link>
-                                    <button onClick={() => setShowModal(true)}>DELETE</button>
-                                    {showModal && (
-                                        <div className='delete-product-confirm-modal'>
-                                            <Modal onClose={() => setShowModal(false)} id={product.id}>
-                                                <DeleteProductConf onClose={() => setShowModal(false)} id={product.id} />
-                                            </Modal>
-                                        </div>
-                                    )}
-                                </div>
-                            </div>
+                            <UserProductInd product={product} />
                         ))
                     )}
                     {userProducts.length === 0 && (
