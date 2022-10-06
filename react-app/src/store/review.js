caaef4de3d8e2cd52653c205012e283a5960e3fe
fangruz114 <101694480+fangruz114@users.noreply.github.com>
2022-10-05 23:11:15 -0700

    review feature backend frontend-product-detail-page

--- react-app/src/store/review.js
+++ react-app/src/store/review.js
@@ -32,3 +32,105 @@ export const getProductReviews = (id) => async dispatch => {
     }
     return data;
 }
+
+export const getUserReviews = () => async dispatch => {
+    const res = await fetch(`/api/reviews`);
+    const data = await res.json();
+    if (res.ok) {
+        await dispatch(loadUserReviews(data.reviews));
+    }
+    return data;
+}
+
+export const addReview = (newReview, productId) => async dispatch => {
+    const { rating, title, content } = newReview;
+    const response = await fetch(`/api/reviews/products/${productId}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            rating,
+            title,
+            content,
+        }),
+    });
+    if (response.ok) {
+        const data = await response.json();
+        await dispatch(createReview(data));
+        return null;
+    } else if (response.status < 500) {
+        const data = await response.json();
+        if (data.errors) {
+            return data.errors;
+        }
+    } else {
+        return ["An error occurred. Please try again."];
+    }
+}
+
+export const editReview = (id, review) => async dispatch => {
+    const { rating, title, content } = review
+    const response = await fetch(`/api/reviews/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            rating,
+            title,
+            content,
+        }),
+    });
+    if (response.ok) {
+        const data = await response.json();
+        await dispatch(createReview(data));
+        return null;
+    } else if (response.status < 500) {
+        const data = await response.json();
+        if (data.errors) {
+            return data.errors;
+        }
+    } else {
+        return ["An error occurred. Please try again."];
+    }
+}
+
+export const removeReview = (id) => async dispatch => {
+    const response = await fetch(`/api/reviews/${id}`, {
+        method: 'DELETE'
+    });
+    const data = response.json();
+    if (response.ok) {
+        await dispatch(deleteReview(id))
+    }
+    return data;
+}
+
+const initialState = {};
+
+const reviewReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type) {
+        case LOAD_PRODUCT_REVIEW:
+            newState = {};
+            action.payload.map(review => newState[review.id] = review);
+            return newState;
+        case LOAD_USER_REVIEW:
+            newState = {};
+            action.payload.map(review => newState[review.id] = review);
+            return newState;
+        case CREATE_REVIEW:
+            newState = Object.assign({}, state);
+            newState[action.payload.id] = action.payload;
+            return newState;
+        case DELETE_REVIEW:
+            newState = Object.assign({}, state);
+            delete newState[action.id];
+            return newState;
+        default:
+            return state;
+    }
+};
+
+export default reviewReducer;
